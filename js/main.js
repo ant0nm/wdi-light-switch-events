ff351321c97ea8c4c912a03d7ffaef0f42eda738
Anton <anton.mois33v@gmail.com>
2019-03-27 14:43:18 -0400

    Light switch implemented

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const statusHeading = document.querySelector('.status');
+  statusHeading.innerText = "FLIP THE SWITCH MAN, IT'S TOO BRIGHT!";
+  const lightSwitch = document.querySelector('button');
+  const body = document.querySelector('body');
 
+  lightSwitch.addEventListener('click', flipSwitch);
 
+  // function declarations
+  function flipSwitch(event) {
+    if (event.target.classList.contains('on')) {
+      event.target.classList.remove('on');
+      event.target.classList.add('off');
+      body.className = 'dark';
+      statusHeading.innerText = "Hey, who turned off the lights?";
+    } else {
+      event.target.classList.remove('off');
+      event.target.classList.add('on');
+      body.className = 'light';
+      statusHeading.innerText = "It's so bright in here!";
+    }
+  }
+
+});
